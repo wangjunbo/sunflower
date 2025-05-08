95a79f83912a3cae24192ebd3232ab82ae940074
wangjunbo <1257680563@qq.com>
2025-05-08 11:00:32 +0800

    调整主界面

--- app/src/main/java/com/google/samples/apps/sunflower/GardenActivity.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/GardenActivity.kt
@@ -1,27 +1,7 @@
-/*
- * Copyright 2018 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package com.google.samples.apps.sunflower
-
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import com.google.samples.apps.sunflower.compose.SunflowerApp
-import com.google.samples.apps.sunflower.ui.SunflowerTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -30,13 +10,13 @@ class GardenActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Displaying edge-to-edge
-        enableEdgeToEdge()
-        setContent {
-            SunflowerTheme {
-                SunflowerApp()
-            }
-        }
-        
+        // 创建WebView并加载URL
+        val webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient() // 确保网页在WebView中打开
+        webView.loadUrl("https://t.ddz.cool/?room=wang1991")
+
+        // 设置WebView为Activity的内容视图
+        setContentView(webView)
     }
 }
